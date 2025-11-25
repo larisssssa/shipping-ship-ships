561961ca83230aa990f11892da9c3dbac2908122
Larissa Ferreira <larissadferr@gmail.com>
2025-11-25 14:45:41 -0600

    databse - added haulers

--- scripts/database.js
+++ scripts/database.js
@@ -4,9 +4,20 @@ const database = {
         { id: 2, location: "Busan, South Korea", volume: "21.6" },
         { id: 3, location: "Rotterdam, The Netherlands", volume: "14.35" },
         { id: 4, location: "Antwerp, Belgium", volume: "12.04" }
+    ],
+    haulers: [
+        { id: 1, dock: 4, name: "Antship" },
+        { id: 2, dock: 3, name: "Benship" },
+        { id: 3, dock: 2, name: "Colship" },
+        { id: 4, dock: 1, name: "Daphship" },
+        { id: 5, dock: 1, name: "Elship" }
     ]
 }
 
 export const getDocks = () => {
     return structuredClone(database.docks)
+}
+
+export const getHaulers = () => {
+    return structuredClone(database.haulers)
 }
