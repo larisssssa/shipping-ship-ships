bd81f9e2a6f2ad1ef1e44f37b6aee86c491e90d2
Larissa Ferreira <larissadferr@gmail.com>
2025-11-25 15:04:44 -0600

    main - header change

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ import { ShippingShipsList } from "./shippingShips.js"
 const mainContainer = document.querySelector("#container")
 
 const appHTML = `
-<h1>Shipping Ship Ships</h1>
+<h1>Shipping Ship Tracker</h1>
 <article class="dept">
     <section class="dept--docks list">
         <h2>Docks</h2>
