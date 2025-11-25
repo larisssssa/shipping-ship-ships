490ea9e17491fa456d363baa29e036770c200156
Larissa Ferreira <larissadferr@gmail.com>
2025-11-25 15:47:08 -0600

    shipHaulers - added eventListener

--- scripts/shipHaulers.js
+++ scripts/shipHaulers.js
@@ -1,16 +1,31 @@
-import { getHaulingShips } from "./database.js"
+import { getHaulingShips, getShippingShips } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.dataset.type === "hauler") {
+            let shipsInHauler = getShippingShips().filter(ship => (itemClicked.dataset.haulerid) ==(ship.haulerId))
+            window.alert(`This hauler is carrying ${shipsInHauler.length} shipping ships`)
+        }
+    }
+)
+
 
 
 export const HaulerList = () => {
     const haulers = getHaulingShips()
 
-    let haulersHTML = "<ul>"
+    let haulersHTML = `<ul data-type="haulers">`
 
     for (const hauler of haulers) {
-        haulersHTML += `<li>${hauler.name}</li>`
+        haulersHTML += `<li data-type="hauler" 
+                            data-haulerid="${hauler.id}"
+                            data-dockid="${hauler.dockId}">${hauler.name}</li>`
     }
 
-    haulersHTML += "</ul>"
+    haulersHTML += `</ul>`
 
     return haulersHTML
 }
