d2663ca2feb9034027866b8716cdbdc61eb7bfb2
Larissa Ferreira <larissadferr@gmail.com>
2025-11-25 14:59:53 -0600

    database - added shippingships

--- scripts/database.js
+++ scripts/database.js
@@ -6,11 +6,25 @@ const database = {
         { id: 4, location: "Antwerp, Belgium", volume: "12.04" }
     ],
     haulers: [
-        { id: 1, dock: 4, name: "Antship" },
-        { id: 2, dock: 3, name: "Benship" },
-        { id: 3, dock: 2, name: "Colship" },
-        { id: 4, dock: 1, name: "Daphship" },
-        { id: 5, dock: 1, name: "Elship" }
+        { id: 1, dockId: 4, name: "Antship" },
+        { id: 2, dockId: 3, name: "Benship" },
+        { id: 3, dockId: 2, name: "Colship" },
+        { id: 4, dockId: 1, name: "Daphship" },
+        { id: 5, dockId: 1, name: "Elship" }
+    ],
+    shippingShips: [
+        {id: 1, haulerId: 1, name: "Acargo"},
+        {id: 2, haulerId: 2, name: "Bcargo"},
+        {id: 3, haulerId: 3, name: "Ccargo"},
+        {id: 4, haulerId: 4, name: "Dcargo"},
+        {id: 5, haulerId: 5, name: "Ecargo"},
+        {id: 6, haulerId: 1, name: "Fcargo"},
+        {id: 7, haulerId: 2, name: "Gcargo"},
+        {id: 8, haulerId: 3, name: "Hcargo"},
+        {id: 9, haulerId: 4, name: "Icargo"},
+        {id: 10, haulerId: 5, name: "Jcargo"},
+        {id: 11, haulerId: 1, name: "Kcargo"},
+        {id: 12, haulerId: 2, name: "Lcargo"}
     ]
 }
 
@@ -20,4 +34,8 @@ export const getDocks = () => {
 
 export const getHaulingShips = () => {
     return structuredClone(database.haulers)
+}
+
+export const getShippingShips = () => {
+    return structuredClone(database.shippingShips)
 }
