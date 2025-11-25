0b35dc0bf97c55690db90a4dd5f2eeae463195d8
Larissa Ferreira <larissadferr@gmail.com>
2025-11-25 15:54:07 -0600

    shippingShips - added data attributes to list items

--- scripts/shippingShips.js
+++ scripts/shippingShips.js
@@ -1,13 +1,16 @@
 import { getShippingShips } from "./database.js"
 
 
+
 export const ShippingShipsList = () => {
     const shippingShips = getShippingShips()
 
     let shippingShipsHTML = "<ul>"
 
     for (const ship of shippingShips) {
-        shippingShipsHTML += `<li>${ship.name}</li>`
+        shippingShipsHTML += `<li data-type="ship"
+                                data-shipid="${ship.id}"
+                                data-haulerid="${ship.haulerId}">${ship.name}</li>`
     }
 
     shippingShipsHTML += "</ul>"
