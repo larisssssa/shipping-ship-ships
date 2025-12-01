5550e2aa5b28d28a7b0d0510329734777ab5c724
Larissa Ferreira <larissadferr@gmail.com>
2025-12-01 09:27:31 -0600

    docks - added metadata to dock items

--- scripts/docks.js
+++ scripts/docks.js
@@ -7,7 +7,7 @@ export const DockList = () => {
     let docksHTML = "<ul>"
 
     for (const dock of docks) {
-        docksHTML += `<li>${dock.location} can hold ${dock.volume} million tons of cargo</li>`
+        docksHTML += `<li data-type="dock" data-dockid="${dock.id}">${dock.location} can hold ${dock.volume} million tons of cargo</li>`
     }
 
     docksHTML += "</ul>"
