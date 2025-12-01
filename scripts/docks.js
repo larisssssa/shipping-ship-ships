9da1cdca1686c9d96b390acd275254b812fae006
Larissa Ferreira <larissadferr@gmail.com>
2025-12-01 09:48:01 -0600

    docks - complete event listener that displays haulers being serviced by dock

--- scripts/docks.js
+++ scripts/docks.js
@@ -1,5 +1,26 @@
-import { getDocks } from "./database.js"
+import { getDocks, getHaulingShips } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.dataset.type === "dock") {
+            const haulers = getHaulingShips()
+            const haulersAtDock = (haulers.filter(hauler => (hauler.dockId) === parseInt(itemClicked.dataset.dockid)))
+            let haulerNames = []
+            if (haulersAtDock.length >= 1) {
+                for (const h of haulersAtDock) {
+                    haulerNames.push(h.name)
+                }
+                window.alert(`${itemClicked.dataset.docklocation} dock is currently unloading ${haulerNames.join(`, `)}.`)
+            } else {
+                window.alert(`${itemClicked.dataset.docklocation} dock is currently unloading nothing.`)
+            }
+            
+        }
+    }
+)
 
 export const DockList = () => {
     const docks = getDocks()
@@ -7,7 +28,7 @@ export const DockList = () => {
     let docksHTML = "<ul>"
 
     for (const dock of docks) {
-        docksHTML += `<li data-type="dock" data-dockid="${dock.id}">${dock.location} can hold ${dock.volume} million tons of cargo</li>`
+        docksHTML += `<li data-type="dock" data-dockid="${dock.id}" data-docklocation="${dock.location}">${dock.location} can hold ${dock.volume} million tons of cargo</li>`
     }
 
     docksHTML += "</ul>"
